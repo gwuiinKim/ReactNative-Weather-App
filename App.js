8af31e077eafb34ab1d4341fb8f2dd35f1bc1b27
gwuiinkim <gwuiinkim@gmail.com>
2019-05-10 00:33:40 +0900

    getting the weather

--- App.js
+++ App.js
@@ -1,31 +1,44 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View, StatusBar } from "react-native";
-import Weather from "./weather";
+import Weather from "./Weather";
+
+const API_KEY = "1ae6e5509a0d8bd42e294904a6dd928a";
 
 export default class App extends Component {
   state = {
     isLoaded: false,
     lat: null,
-    long: null
+    long: null,
+    error: null
   };
-
   componentDidMount = () => {
     navigator.geolocation.getCurrentPosition(
       position => {
-        this.setState({
-          lat: position.coords.latitude,
-          long: position.coords.longitude,
-          isLoaded: true
-        });
+        this._getWeather(position.coords.latitude, position.coords.longitude);
       },
       error => {
         console.log(error);
+        this.setState({
+          error
+        });
       }
     );
   };
+  _getWeather = (lat, lon) => {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`
+    )
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          temperature: json.main.temp,
+          name: json.weather[0].main,
+          isLoaded: true
+        });
+      });
+  };
   render() {
-    const { isLoaded } = this.state;
-
+    const { isLoaded, error } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar hidden={true} />
@@ -33,7 +46,8 @@ export default class App extends Component {
           <Weather />
         ) : (
           <View style={styles.loading}>
-            <Text style={styles.loadingText}>Getting the weather..</Text>
+            <Text style={styles.loadingText}>Getting the fucking weather</Text>
+            {error ? <Text>{error.message}</Text> : null}
           </View>
         )}
       </View>
@@ -48,12 +62,12 @@ const styles = StyleSheet.create({
   },
   loading: {
     flex: 1,
-    backgroundColor: "#C1DC89",
+    backgroundColor: "#FDF6AA",
     justifyContent: "flex-end",
-    paddingLeft: 20
+    paddingLeft: 25
   },
   loadingText: {
-    marginBottom: 100,
-    fontSize: 40
+    fontSize: 38,
+    marginBottom: 100
   }
 });
